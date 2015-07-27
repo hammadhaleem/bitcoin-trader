41e90d2d214a2b8f350ba7dcb711889b67f10a91
PC2 <PC2@PC2>
2015-07-27 09:58:46 +0800

    FD

--- traderProg.py
+++ traderProg.py
@@ -6,17 +6,24 @@ import exchangeLayer as el
 import traderProg
 import genconfig as gc
 
-buy_pool = []
-sell_pool = []
+
+number_of_trades = 10 
+
 @asyncio.coroutine
 def TradeWrapper():
 	while True:
-	  	print(el.GetFree('currency'))
-	  	print(el.GetFrozen('currency'))
-	  	print(el.GetMarketPrice('ask'))
-	  	print (buy_pool)
-	  	print (sell_pool)
+		free_money  = el.GetFree('currency')
 
-	  	buy_pool.append(el.GetMarketPrice('ask'))
-	  	sell_pool.append(el.GetMarketPrice('bid'))
-	  	yield from asyncio.sleep(gc.Trader.ReIssueDelay)
+		frozen_money = el.GetFrozen('currency')
+		frozen_btc = el.GetFrozen('asset')
+
+		market_price = el.GetMarketPrice('ask')
+	
+		sell_price = el.GetMarketPrice('ask')
+		buy_price  = el.GetMarketPrice('bid')
+
+		if( frozen_money <= 0.001 and frozen_btc <= 0.001):
+			print(str(sell_price) + "\t" + str(buy_price))
+		else:
+			print ("Still frozen asserts")
+		yield from asyncio.sleep(gc.Trader.ReIssueDelay)
