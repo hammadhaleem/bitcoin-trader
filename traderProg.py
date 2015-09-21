a9c877f0bc72f7723527196386a77702ec6eb0f6
hammad <hhaleem@connect.ust.hk>
2015-09-21 04:03:21 -0400

    average out

--- traderProg.py
+++ traderProg.py
@@ -7,8 +7,8 @@ import traderProg
 import genconfig as gc
 import time 
 
-number_of_trades = 20
-diff = 0.012
+number_of_trades = 5
+diff = 0.01
 
 def get_trades( sell_price , buy_price,  parts , free_money , free_coin):
 	if ( sell_price == None or buy_price == None):
@@ -27,19 +27,38 @@ def get_trades( sell_price , buy_price,  parts , free_money , free_coin):
 
 @asyncio.coroutine
 def TradeWrapper():
+	count = -4
 	while True:
+		count = count + 1
+		if count % 10 is 0 :
+			val = (float(el.GetFree('currency')) + float(el.GetFrozen('currency'))) + (float(el.GetFree('asset')) + float(el.GetFrozen('asset'))) * float(el.GetMarketPrice('ask'))
+			print("[info-wc]\tFree Money :{0:5}\tFree Coin :{1:5}\tAsserts :{2:5}\tMarket price :{3:5}".format(el.GetFree('currency') , el.GetFree('asset') , val ,el.GetMarketPrice('ask'))) 
+			count = -4
 		free_money  = el.GetFree('currency')
 		free_coin   = el.GetFree('asset')
 
 		frozen_money = el.GetFrozen('currency')
 		frozen_btc = el.GetFrozen('asset')
-
+		avg_price= 0
+		counter = 0
 		market_price = el.GetMarketPrice('ask')
-	
+		if market_price is not None  :
+			while counter != 30:
+				try:
+					market_price = el.GetMarketPrice('ask')
+					avg_price = avg_price + market_price 
+					counter = counter +1
+					time.sleep(0.1)
+				except Exception as e :
+					print ("[Exception]\t"+str(e))
+					time.sleep(1)
+					pass
+			market_price = avg_price / counter
+		
 		sell_price = el.GetMarketPrice('ask')
 		buy_price  = el.GetMarketPrice('bid')
 		to_issue_trades = get_trades( market_price, market_price , number_of_trades , free_money , free_coin )
-		if( frozen_money <= 0.001 and frozen_btc <= 0.001):
+		if( frozen_money <= 1 and frozen_btc <= 0.001):
 			for trades in to_issue_trades : 
 				try:
 					if (trades[0] < 0 ):
